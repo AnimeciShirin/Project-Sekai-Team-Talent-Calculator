e29fb847d1db264ccd773b25094a6d381431da19
shirin404 <sekercika.transformice@gmail.com>
2024-03-05 18:45:49 +0300

    Update methods.py

--- methods.py
+++ methods.py
@@ -2,16 +2,6 @@ from Rrich import print, input
 from classes import characters
 import sys
 
-def get_variable_name(variable):
-    for name in globals():
-        if id(globals()[name]) == id(variable):
-            return name
-    for name in locals():
-        if id(locals()[name]) == id(variable):
-            return name
-    return None
-
-
 # Cleaning
 for n in dir(characters):
     if not callable(getattr(characters, n)) and not n.startswith("__") and not type(getattr(characters, n)) == list:
