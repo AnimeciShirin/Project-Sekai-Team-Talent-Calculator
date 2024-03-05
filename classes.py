69b2fb0976c6a6cf55b9297eb291046902117249
shirin404 <sekercika.transformice@gmail.com>
2024-03-05 18:42:57 +0300

    EN version + fix

--- classes.py
+++ classes.py
@@ -1,11 +1,11 @@
 class characters:
 # Virtual Singer TEAM
-    miku = {"key":"hm", "name":"Hatsune Miku", "team":"virtualS", "color":"#33CCBA"}
-    rin = {"key":"kr", "name":"Kagamine Rin", "team":"virtualS", "color":"#FFCC11"} # ღ
-    len = {"key":"kl", "name":"Kagamine Len", "team":"virtualS", "color":"#FFEE12"}
-    luka = {"key":"ml", "name":"Megurine Luka", "team":"virtualS", "color":"#FFBACC"} # ღღ    
-    meiko = {"key":"mei", "name":"MEIKO", "team":"virtualS", "color":"#DE4444"}
-    kaito = {"key":"kai", "name":"KAITO", "team":"virtualS", "color":"#3367CD"}
+    miku = {"key":"hm", "name":"Hatsune Miku", "team":"virtualS", "sub":"leoNeed", "color":"#33CCBA"}
+    rin = {"key":"kr", "name":"Kagamine Rin", "team":"virtualS", "sub":"", "color":"#FFCC11"} # ღ
+    len = {"key":"kl", "name":"Kagamine Len", "team":"virtualS", "sub":"", "color":"#FFEE12"}
+    luka = {"key":"ml", "name":"Megurine Luka", "team":"virtualS", "sub":"", "color":"#FFBACC"} # ღღ    
+    meiko = {"key":"mei", "name":"MEIKO", "team":"virtualS", "sub":"", "color":"#DE4444"}
+    kaito = {"key":"kai", "name":"KAITO", "team":"virtualS", "sub":"", "color":"#3367CD"}
 
 # Leo*╯Need
     ichika = {"key":"hi", "name":"Hoshino Ichika", "team":"leoNeed", "color":"#33AAEE"} # ღღ
@@ -35,7 +35,7 @@ class characters:
     kanade = {"key":"yk", "name":"Yoisaki Kanade", "team":"nightcord25", "color":"#BB6588"}
     mafuyu = {"key":"amu", "name":"Asahina Mafuyu", "team":"nightcord25", "color":"#8889CC"}
     ena = {"key":"se", "name":"Shinonome Ena", "team":"nightcord25", "color":"#CCAA87"}
-    mizuki = {"key":"ami", "name":"Akiyana Mizuki", "team":"nightcord25", "color":"#E4A8CA"}
+    mizuki = {"key":"ami", "name":"Akiyama Mizuki", "team":"nightcord25", "color":"#E4A8CA"}
 
 # Global List
     chars = [miku, rin, len, luka, kaito, meiko, ichika, saki, honami, shiho, minori, haruka, airi, shizuku, kohane, an, akito, toya, tsukasa, emu, nene, rui, kanade, mafuyu, ena, mizuki]
--- main.py
+++ main.py
@@ -1,16 +1,46 @@
 from classes import characters
-from methods import komut
 from Rrich import print, input
+from methods import calculate
+import argparse
 
 print(f"[bold gray50]#####################################################################################[/bold gray50]")
 print(f"[bold gray50]#[/bold gray50]                                                                                   [bold gray50]#[/bold gray50]")
-print(f"[bold gray50]#[/bold gray50]                           [bold][cyan]Project[/cyan] [yellow]Sekai[/yellow][/bold] [deep_pink4]Talent Hesaplayıcı[/deep_pink4]                        [bold gray50]#[/bold gray50]")
+print(f"[bold gray50]#[/bold gray50]                           [bold][cyan]Project[/cyan] [yellow]Sekai[/yellow][/bold] [deep_pink4]Talent Calculator[/deep_pink4]                         [bold gray50]#[/bold gray50]")
 print(f"[bold gray50]#[/bold gray50]                                                                                   [bold gray50]#[/bold gray50]")
 print(f"[bold gray50]#[/bold gray50]                                    [bold green]Creator:[/bold green] [yellow]Shirin[/yellow]                                [bold gray50]#[/bold gray50]")
 print(f"[bold gray50]#[/bold gray50]                                                                                   [bold gray50]#[/bold gray50]")
-print(f"[bold gray50]#[/bold gray50]           [bold cyan]Dekorasyonlar dahil[/bold cyan] | bir başka Project Sekai utiliy yazılımı           [bold gray50]#[/bold gray50]")
+print(f"[bold gray50]#[/bold gray50]              [bold cyan]Including accesories[/bold cyan] | another PJSK utility application              [bold gray50]#[/bold gray50]")
 print(f"[bold gray50]#[/bold gray50]                                                                                   [bold gray50]#[/bold gray50]")
 print(f"[bold gray50]#####################################################################################[/bold gray50]")
 print("")
 
-komut()
+parser = argparse.ArgumentParser(description="Calculate talent points")
+# parser.add_argument("-i", "--input_file", required=True, help="Input video file.")
+
+parser.add_argument("-c", "--characters", required=True, help="5 character input. Example: hm,kn,sai,krui,oe")
+parser.add_argument("-g", "--sub_group", help="Virtual Singer subgroup input. Can take multiple input. If input amount isn't equal to Virtual Singer amount; you will get error. Example: leo,vivid")
+parser.add_argument("-r", "--rarity", required=True, help="Char rarity input. Birtday = 0. Example: 0,3,3,4,4")
+parser.add_argument("-m", "--mastery", required=True, help="Mastery input. Example: 0,1,2,5,5")
+parser.add_argument("-s", "--side_story", required=True, help="Side story input. Closed = 0, Level = 1 or 2. Example: 0,0,1,2,2")
+parser.add_argument("-a", "--accesory", required=True, help="Character accesory input. Buff percent. Example: 2,5,15,20,18")
+parser.add_argument("-t", "--team_accesory", required=True, help="Team accessory input. Buff percent. Example: 2.5,1.5,3,0.5,1")
+parser.add_argument("-e", "--element_accesory", required=True, help="Type accesory. Buff percent. Example: 2.5,1.5,3,0.5,1")
+parser.add_argument("-b", "--talent_bonus", required=True, help="Character rank talent bonus. Buff percent. Example: 1.5,2.0,2.5,3.1,1.9")
+parser.add_argument("-x", "--all_same_type", required=True, help="Is all the types same? Type y or n.")
+parser.add_argument("-q", "--all_same_team", required=True, help="Is all the teams same? Type y or n.")
+
+args = parser.parse_args()
+
+calculate(
+    args.characters,
+    args.sub_group,
+    args.rarity,
+    args.mastery,
+    args.side_story,
+    args.accesory,
+    args.element_accesory,
+    args.team_accesory,
+    args.talent_bonus,
+    args.all_same_type,
+    args.all_same_team
+)
